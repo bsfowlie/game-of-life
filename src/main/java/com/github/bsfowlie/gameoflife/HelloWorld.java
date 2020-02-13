a4bb1002fb719cc39e3ae31a4322fe91c23ca513
Ben Fowlie <bsfowlie@gmail.com>
2020-02-12 23:14:01 -0500

    Initial commit

---
+++ src/main/java/com/github/bsfowlie/gameoflife/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.github.bsfowlie.gameoflife;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+    public static void main(String... args) {
+        Logger logger=Logger.getLogger(HelloWorld.class.getName());
+        logger.info("This is a Hello World!");
+    }
+}
