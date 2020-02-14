7f0836fa6b4a2d605ff594da62fb82a3c0804c70
Ben Fowlie <bsfowlie@gmail.com>
2020-02-13 22:50:51 -0500

    Add display names

--- src/test/java/com/github/bsfowlie/gameoflife/CellShould.java
+++ src/test/java/com/github/bsfowlie/gameoflife/CellShould.java
@@ -1,11 +1,14 @@
 package com.github.bsfowlie.gameoflife;
 
 import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Cell should")
 public final class CellShould implements WithAssertions {
 
     @Test
+    @DisplayName("not allow construction with a null State")
     public void notAllowConstructionWithNullState() {
         assertThatIllegalStateException().isThrownBy(()-> new Cell(null));
     }
--- src/test/java/com/github/bsfowlie/gameoflife/MainShould.java
+++ src/test/java/com/github/bsfowlie/gameoflife/MainShould.java
@@ -4,17 +4,19 @@ import static org.mockito.Mockito.verify;
 
 import java.util.logging.Logger;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
+@DisplayName("Main should")
 class MainShould {
 
-
     @Test
-    void shouldLog(@Mock Logger logger) {
+    @DisplayName("output to the logger")
+    void outputTo(@Mock Logger logger) {
 
         Main.LOGGER = logger;
         Main.main();
