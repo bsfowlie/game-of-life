3d23f53347f398b3fe600d68c066a7b8b11b5af3
Ben Fowlie <bsfowlie@gmail.com>
2020-02-13 22:50:09 -0500

    Parameterize the tests for next state

--- src/test/java/com/github/bsfowlie/gameoflife/AliveCellShould.java
+++ src/test/java/com/github/bsfowlie/gameoflife/AliveCellShould.java
@@ -1,55 +1,29 @@
 package com.github.bsfowlie.gameoflife;
 
 import org.assertj.core.api.WithAssertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayName("Alive Cell should")
 public final class AliveCellShould implements WithAssertions {
 
     private final Cell cell = new Cell(Cell.State.ALIVE);
 
-    @Test
-    public void dieWithZeroNeighbors() {
-        assertThat(cell.nextState(0)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void dieWithOnlyOneNeighbor() {
-        assertThat(cell.nextState(1)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void liveWithTwoNeighbors() {
-        assertThat(cell.nextState(2)).isEqualTo(Cell.State.ALIVE);
-    }
-
-    @Test
-    public void liveWithThreeNeighbors() {
-        assertThat(cell.nextState(3)).isEqualTo(Cell.State.ALIVE);
-    }
-
-    @Test
-    public void dieWithFourNeighbors() {
-        assertThat(cell.nextState(4)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void dieWithFiveNeighbors() {
-        assertThat(cell.nextState(5)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void dieWithSixNeighbors() {
-        assertThat(cell.nextState(6)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void dieWithSevenNeighbors() {
-        assertThat(cell.nextState(7)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void dieWithEightNeighbors() {
-        assertThat(cell.nextState(8)).isEqualTo(Cell.State.DEAD);
+    @ParameterizedTest(name="become {1} with {0} neighbors")
+    @CsvSource({
+            "0, DEAD",
+            "1, DEAD",
+            "2, ALIVE",
+            "3, ALIVE",
+            "4, DEAD",
+            "5, DEAD",
+            "6, DEAD",
+            "7, DEAD",
+            "8, DEAD"
+    })
+    public void becomeWithNeighbors(int aliveNeighbors, Cell.State expected) {
+        assertThat(cell.nextState(aliveNeighbors)).isEqualTo(expected);
     }
 
 }
--- src/test/java/com/github/bsfowlie/gameoflife/DeadCellShould.java
+++ src/test/java/com/github/bsfowlie/gameoflife/DeadCellShould.java
@@ -1,55 +1,29 @@
 package com.github.bsfowlie.gameoflife;
 
 import org.assertj.core.api.WithAssertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+@DisplayName("Dead Cell should")
 public final class DeadCellShould implements WithAssertions {
 
     private final Cell cell = new Cell(Cell.State.DEAD);
 
-    @Test
-    public void stayDeadWithZeroNeighbors() {
-        assertThat(cell.nextState(0)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void stayDeadWithOnlyOneNeighbor() {
-        assertThat(cell.nextState(1)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void stayDeadWithTwoNeighbors() {
-        assertThat(cell.nextState(2)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void liveWithThreeNeighbors() {
-        assertThat(cell.nextState(3)).isEqualTo(Cell.State.ALIVE);
-    }
-
-    @Test
-    public void stayDeadWithFourNeighbors() {
-        assertThat(cell.nextState(4)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void stayDeadWithFiveNeighbors() {
-        assertThat(cell.nextState(5)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void stayDeadWithSixNeighbors() {
-        assertThat(cell.nextState(6)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void stayDeadWithSevenNeighbors() {
-        assertThat(cell.nextState(7)).isEqualTo(Cell.State.DEAD);
-    }
-
-    @Test
-    public void stayDeadWithEightNeighbors() {
-        assertThat(cell.nextState(8)).isEqualTo(Cell.State.DEAD);
+    @ParameterizedTest(name="become {1} with {0} neighbors")
+    @CsvSource({
+            "0, DEAD",
+            "1, DEAD",
+            "2, DEAD",
+            "3, ALIVE",
+            "4, DEAD",
+            "5, DEAD",
+            "6, DEAD",
+            "7, DEAD",
+            "8, DEAD"
+    })
+    public void becomeWithNeighbors(int aliveNeighbors, Cell.State expected) {
+        assertThat(cell.nextState(aliveNeighbors)).isEqualTo(expected);
     }
 
 }
